fabbb254b067f98cdc01cb155b61ee44b421e419
BabakhalilM <babakhalilmalyam2@gmail.com>
2024-08-19 22:20:25 +0530

    Frontend link add

--- EventManager/Event_frontend/src/components/login-signup/Home.jsx
+++ EventManager/Event_frontend/src/components/login-signup/Home.jsx
@@ -8,6 +8,11 @@ const Home = () => {
       <Heading letterSpacing={1}>Welcome to Event Management</Heading>
       <Text>Login to See And Register Different Events we are Condecting</Text>
       <Text><strong>Note:-</strong> Login to Create Event With admin Details</Text>
+      <details>
+                  <summary>If you want to check the page functionality </summary>
+                  <Text mt={2}><strong>Email:-</strong> admin@gmail.com</Text>
+                  <Text><strong>password:</strong>123@admin</Text>
+                </details>
     </Box>
   )
 }
