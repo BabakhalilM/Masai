0c102fac18d52140abe1c1ef7357f743122c3d79
BabakhalilM <babakhalilmalyam2@gmail.com>
2024-08-19 18:02:39 +0530

    Frontend link add

--- EventManager/Event_frontend/src/components/login-signup/api.jsx
+++ EventManager/Event_frontend/src/components/login-signup/api.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://pollify-yc1z.onrender.com/api',
-  baseURL:"http://localhost:2300/api/",
+  // baseURL:"http://localhost:2300/api/",
+  baseURL:"https://event-manager-p6m5.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
